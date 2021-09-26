7de7f70fb8457aa03bc949cb0fa861559cf43ab6
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-09-26 05:21:30 -0300

    Propriedades no React

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,42 +1,30 @@
-let repositoryName = ''
+import { RepositoryItem } from "./RepositoryItem"
+
+const repository = {
+    name: 'Gustavo032',
+    description: 'readme interativo',
+    link: 'https://github.com/Gustavo032/Gustavo032'
+}
 
 export function RepositoryList(){
     return(
         <section class="repository-list">
-            <h2>Lista dos meus repositórios</h2>
+            <h1>Lista dos meus repositórios</h1>
+
             <hr />
             <ul>
-            {repositoryName = '1'}
-                <li>
-                    <strong>Nome do Repositório {repositoryName}</strong>
-                    <p>Css</p>
-                    <a href="">Acesse o Repositório</a>
-                </li>
-            </ul>
-            
+
+             <RepositoryItem repository={repository}/>
             <hr />
 
-            <ul>
-            {repositoryName = '2'}
-                <li>
-                    <strong>Nome do Repositório {repositoryName}</strong>
-                    <p>React</p>
-                    <a href="">Acesse o Repositório</a>
-                </li>
-            </ul>
-            
+            <RepositoryItem repository={repository}/>
             <hr />
 
-            <ul>
-            {repositoryName = '3'}
-                <li>
-                    <strong>Nome do Repositório {repositoryName}</strong>
-                    <p>Javascript</p>
-                    <a href="">Acesse o Repositório</a>
-                </li>
+            <RepositoryItem repository={repository}/>
+            <hr />
+
             </ul>
             
-            <hr />
 
         </section>
     )
