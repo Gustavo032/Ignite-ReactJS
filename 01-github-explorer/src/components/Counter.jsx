908b57d93f36900c0dc5372090dd4aa53e195aa0
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-09-27 21:58:26 -0300

    explicando imutabilidade

--- 01-github-explorer/src/components/Counter.jsx
+++ 01-github-explorer/src/components/Counter.jsx
@@ -3,10 +3,13 @@ import { useState } from 'react';
 // hook = gancho}
 
 export function Counter(){
-    const [counter, setCounter] = useState(0);
- // let it change
-// deixe isso mudar
+    // * curiosidade *
+        // let it change
+       // deixe isso mudar
 
+x
+    const [counter, setCounter] = useState(0);
+    // usa o setCounter pra mudar o valor de Counter e useState pra designar o valor atual
     function increment() {
         setCounter(counter +1);
     };
@@ -18,3 +21,18 @@ export function Counter(){
         </div>
     );
 }
+
+
+// * imutábilidade *
+    // errado:
+        // alterando a informação na memória // mutação
+        // alterando diretamente
+            // usuarios = ["gustavo","Guusta.r","Gustavo Ramos"]
+            // usuarios.push('matheus')
+
+    // certo:
+        // Alterando indiretamente    
+            // usuarios = ["gustavo","Guusta.r","Gustavo Ramos"]
+            // novosUsers = [...usuarios, 'matheus']
+
+        //  setCounter(counter +1);
