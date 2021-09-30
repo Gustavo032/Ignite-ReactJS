07168c7e0b63d91ec56a1dcf2b45da7e28f7c12c
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-09-29 22:55:37 -0300

    arrumando o fast refresh

--- 01-github-explorer/src/components/Counter.jsx
+++ 01-github-explorer/src/components/Counter.jsx
@@ -10,17 +10,15 @@ export function Counter(){
     const [counter, setCounter] = useState(0);
     // usa o setCounter pra mudar o valor de Counter e useState pra designar o valor atual
     function increment() {
-        setCounter(counter +1);
+        setCounter(counter + 1);
     };
 
     return(
         <div>
             <h2>{counter}</h2>
             <button type="button" onClick={increment}>
-                Increment
+                Increment 
             </button>
         </div>
     );
 }
-
-
