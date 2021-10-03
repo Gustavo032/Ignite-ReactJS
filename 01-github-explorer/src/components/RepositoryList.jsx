49eaf76264f75550893403a5b95ce1f2a55f88ed
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-10-03 18:13:40 -0300

    utilizando useEffect

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,6 +1,13 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss';
+
+// use effect = dispara uma função quando algo acontecer
+    // roda tudo do componente que não seja um hook 
+    // roda apenas 1 vez quando não tem o segundo parametro
+
+// https://api.github.com/users/gustavo032/repos
 
 const repository = {
     name: 'Gustavo032',
@@ -9,6 +16,15 @@ const repository = {
 }
 
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(()=>{
+        fetch('https://api.github.com/users/gustavo032/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
+
+    console.log(repositories)
     return(
         <section className="repository-list">
             <h1>Lista dos meus repositórios</h1>
